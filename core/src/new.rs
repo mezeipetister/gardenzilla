286d812fc098c8be90690071c857acc6922809ee
Peter <mezeipetister@gmail.com>
2020-07-20 11:22:45 +0200

    product + upl logic

--- core/src/new.rs
+++ core/src/new.rs
@@ -30,12 +30,19 @@ pub type StockId = u32;
 pub type DeliveryId = u32;
 pub type UplId = u32;
 pub type ProcurementId = u32;
+pub type SourceId = u32;
+pub type InvoiceId = String;
 
 pub enum Unit {
     Piece,
     Millimeter,
     Gram,
     Milliliter,
+}
+
+pub enum Quantity {
+    Simple(u32),
+    Complex(u32, u32),
 }
 
 impl Unit {
@@ -54,9 +61,9 @@ impl Unit {
 pub struct Product {
     sku: SKU,
     name: String,
-    quantity: u32, // 250
-    unit: Unit,    // ml
-    created_by: User,
+    quantity: Quantity, // 250 5x3 ml -> 15 ml => price / ml
+    unit: Unit,         // ml
+    created_by: UserId,
     created_at: DateTime<Utc>,
 }
 
@@ -72,7 +79,7 @@ pub struct Upl {
     procurement_id: ProcurementId,      // todo: maybe ProcurementId?
     net_procurement_price: f32,         // todo: sure?
     best_before: Option<DateTime<Utc>>, // todo: DateTime<Utc> or NaiveDate?
-    quantity: u32,                      // unit is the same as the SKU's
+    quantity: Quantity,                 // unit is the same as the SKU's
     is_opened: bool,
     location: UplLocation,        //
     history: Vec<UplHistoryItem>, // todo: + DateTime per event. Only location change?
@@ -80,7 +87,7 @@ pub struct Upl {
 
 pub struct UplHistoryItem {
     created_at: DateTime<Utc>,
-    created_by: User,
+    created_by: UserId,
     location: UplLocation,
 }
 
@@ -115,26 +122,77 @@ impl UplStore {
     }
 }
 
-pub struct User();
-pub struct Customer();
+pub struct UserId();
+pub struct CustomerId();
 
 pub struct Cart {
     id: u32,
-    customer: Customer,
+    customer: CustomerId,
     items: HashMap<SKU, CartItem>,
     total_net_price: f32,
     total_vat: f32,
     total_gross_price: f32,
-    created_by: User,
+    created_by: UserId,
     created_at: DateTime<Utc>,
     is_closed: bool,
 }
 
 pub struct CartItem {
     sku: SKU,
-    piece: f32,
+    piece: u32,
     net_unit_price: f32,
     vat: f32,
     gross_unit_price: f32,
     upls: Vec<UplNeedle>,
+}
+
+pub struct Purchase {
+    id: u32,
+    items: HashMap<SKU, CartItem>,
+    customer: CustomerId,
+    payment: (),
+    is_payed: bool,
+    document: DocumentKind,
+    document_history: Vec<(DateTime<Utc>, DocumentKind)>,
+    is_removed: bool,
+}
+
+pub enum DocumentKind {
+    Receipt,
+    Normal(InvoiceId),
+    Storno(InvoiceId),
+}
+
+pub struct Invoice {
+    id: String,
+    customer: String,
+}
+
+pub struct Stock {
+    id: StockId,
+    name: String,
+    items: HashMap<SKU, Vec<UplNeedle>>,
+}
+
+pub struct Source {
+    id: SourceId,
+    name: String,
+    items: HashMap<SKU, Vec<SourceItemHistory>>,
+}
+
+pub struct Procurement {
+    id: ProcurementId,
+    description: String,
+    source: SourceId,
+    items: Vec<ProcurementItem>,
+    expected_net_total: f32,
+    created_by: UserId,
+    created_at: DateTime<Utc>,
+}
+
+pub struct ProcurementItem {
+    sku: SKU,
+    quantity: u32,
+    expected_net_unit_price: f32,
+    description: String,
 }
