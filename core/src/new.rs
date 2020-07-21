205eebaeea5693a2314a0fccb0fea6e7030bb405
Peter <mezeipetister@gmail.com>
2020-07-21 10:53:14 +0200

    ..

--- core/src/new.rs
+++ core/src/new.rs
@@ -32,6 +32,8 @@ pub type UplId = u32;
 pub type ProcurementId = u32;
 pub type SourceId = u32;
 pub type InvoiceId = String;
+pub struct UserId();
+pub struct CustomerId();
 
 pub enum Unit {
     Piece,
@@ -43,6 +45,15 @@ pub enum Unit {
 pub enum Quantity {
     Simple(u32),
     Complex(u32, u32),
+}
+
+impl std::fmt::Display for Quantity {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match &self {
+            Quantity::Simple(quantity) => write!(f, "{}", quantity),
+            Quantity::Complex(multiplier, quantity) => write!(f, "{}x{}", multiplier, quantity),
+        }
+    }
 }
 
 impl Unit {
@@ -61,16 +72,19 @@ impl Unit {
 pub struct Product {
     sku: SKU,
     name: String,
-    quantity: Quantity, // 250 5x3 ml -> 15 ml => price / ml
-    unit: Unit,         // ml
+    quantity: Quantity, // e.g.: Simple(u32) => 3 ml, or Complex(u32, u32) => 5x3 ml
+    unit: Unit,         // e.g.: ml
+    net_retail_price: f32, // e.g.: 1000 HUF
+    vat_percentage: u32, // e.g.: 27 => 27%, 0 | 5 | 18 | 27 based on the Hungarian tax law 2020
     created_by: UserId,
     created_at: DateTime<Utc>,
 }
 
 enum UplLocation {
-    Cart(CartId),
     Stock(StockId),
-    Delivery(DeliveryId),
+    //Delivery(DeliveryId),
+    Cart(CartId),
+    Purchase(CartId),
 }
 
 pub struct Upl {
@@ -79,9 +93,10 @@ pub struct Upl {
     procurement_id: ProcurementId,      // todo: maybe ProcurementId?
     net_procurement_price: f32,         // todo: sure?
     best_before: Option<DateTime<Utc>>, // todo: DateTime<Utc> or NaiveDate?
-    quantity: Quantity,                 // unit is the same as the SKU's
-    is_opened: bool,
-    location: UplLocation,        //
+    quantity: Quantity, // todo: Should update all the times, when the SKU quantity updated
+    unit: Unit,         // todo: Should be update all the times, when the SKU unit updated
+    is_unopened: bool,
+    location: UplLocation,
     history: Vec<UplHistoryItem>, // todo: + DateTime per event. Only location change?
 }
 
@@ -91,20 +106,20 @@ pub struct UplHistoryItem {
     location: UplLocation,
 }
 
-pub struct UplNeedle {
+pub struct UplPhdr {
     upl_id: u32,
     sku: SKU,
 }
 
 pub struct UplStore {
-    upls: HashMap<UplId, Upl>, // todo: somehow partitioning?
+    upls: HashMap<UplId, Upl>,
 }
 
 pub trait UplManager {
-    fn move_in(&mut self, upl_needle: UplNeedle) -> &Upl {
+    fn move_in(&mut self, upl_needle: UplPhdr) -> &Upl {
         unimplemented!()
     }
-    fn move_out(&mut self) -> UplNeedle {
+    fn move_out(&mut self) -> UplPhdr {
         unimplemented!()
     }
 }
@@ -122,8 +137,11 @@ impl UplStore {
     }
 }
 
-pub struct UserId();
-pub struct CustomerId();
+pub struct Stock {
+    id: StockId,
+    name: String,
+    items: HashMap<SKU, Vec<UplPhdr>>,
+}
 
 pub struct Cart {
     id: u32,
@@ -143,7 +161,7 @@ pub struct CartItem {
     net_unit_price: f32,
     vat: f32,
     gross_unit_price: f32,
-    upls: Vec<UplNeedle>,
+    upls: Vec<UplPhdr>,
 }
 
 pub struct Purchase {
@@ -166,12 +184,6 @@ pub enum DocumentKind {
 pub struct Invoice {
     id: String,
     customer: String,
-}
-
-pub struct Stock {
-    id: StockId,
-    name: String,
-    items: HashMap<SKU, Vec<UplNeedle>>,
 }
 
 pub struct Source {
